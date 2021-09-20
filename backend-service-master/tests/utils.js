e8e7dc09576638f42733b1359fded8e3b87903c3
ang-jason <angjason896@gmail.com>
2021-09-20 16:49:47 +0800

    make it better

---
+++ backend-service-master/tests/utils.js
@@ -0,0 +1,35 @@
+require('dotenv').config({ path: '.env.test' })
+const App = require('../src/app')
+const Router = require('../src/routes')
+const AuthMiddleware = require('../src/middlewares/auth')
+const AuthService = require('../src/services/auth')
+const AmqpService = require('../src/services/amqp')
+const db = require('../src/db')
+
+const utils = {}
+
+const amqpService = AmqpService()
+const authService = AuthService(db)
+const authMiddleware = AuthMiddleware(authService)
+const router = Router(authMiddleware, authService, amqpService, db)
+const app = App(router)
+
+utils.app = app
+utils.db = db
+
+utils.setup = async () => {
+  await db.initialise()
+  await db.clearItemsTables()
+  await db.clearUsersTables()
+}
+
+utils.teardown = async () => {
+  await db.end()
+}
+
+utils.registerUser = async (username = 'test_user', password = 'test_password') => {
+  const token = await authService.registerUser(username, password)
+  return `Bearer ${token}`
+}
+
+module.exports = utils
